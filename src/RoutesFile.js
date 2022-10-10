a317e01afc31010f642b4b471fcc30075407428d
Nilesh Aggarwal <53069494+nileshaggarwal@users.noreply.github.com>
2022-10-10 10:25:46 +0530

    reactGa implemented

--- src/RoutesFile.js
+++ src/RoutesFile.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import ReactGA from "react-ga";
 import Home from "./pages/home";
 import Redirect from "./pages/Redirect";
 
+const TRACKING_ID = "UA-97201267-2";
+ReactGA.initialize(TRACKING_ID);
+
 const RoutesFile = () => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
